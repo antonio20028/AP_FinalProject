a1605369ce119072d8c329010bffe773e4762728
= <88873852+antonio20028@users.noreply.github.com>
2021-12-05 19:06:26 +0530

    Buttons animations completed

--- WillHero/src/main/java/iiitd/ac/ap_group17/willhero/HomeController.java
+++ WillHero/src/main/java/iiitd/ac/ap_group17/willhero/HomeController.java
@@ -1,12 +1,21 @@
 package iiitd.ac.ap_group17.willhero;
 
 import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
 import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
 import javafx.util.Duration;
+
+import java.io.IOException;
+import java.security.Key;
 
 public class HomeController {
 
@@ -20,14 +29,39 @@ public class HomeController {
     private Label lbl;
 
     @FXML
+    private AnchorPane home;
+
+    @FXML
     private ImageView btnSavedGame;
 
     @FXML
     private ImageView btnOption;
 
     @FXML
-    protected void btnStartNewGameClicked(){
-            startButtonIllusionAnimation(btnNewGame, 115, 255);
+    private AnchorPane gamePane;
+
+    @FXML
+    protected void btnStartNewGameClicked() {
+        startButtonIllusionAnimation(btnNewGame, 115, 255);
+
+       AnchorPane anchorPane = new AnchorPane();
+
+        gamePane = new AnchorPane(home);
+        gamePane.getChildren().add(anchorPane);
+
+        KeyFrame start = new KeyFrame(Duration.ZERO,
+                new KeyValue(gamePane.translateXProperty(), gamePane.getWidth()),
+                new KeyValue(home.translateXProperty(), 0));
+
+        KeyFrame end = new KeyFrame(Duration.seconds(1),
+                new KeyValue(gamePane.translateXProperty(), 0),
+                new KeyValue(home.translateXProperty(), -gamePane.getWidth())
+        );
+
+        Timeline t = new Timeline(start, end);
+        t.play();
+
+
     }
 
 
