32e6ae47ee3c2dc86dc84ee177bec49da2bea4ef
= <88873852+antonio20028@users.noreply.github.com>
2021-12-05 18:22:53 +0530

    Buttons animations completed

--- WillHero/src/main/java/iiitd/ac/ap_group17/willhero/HomeApplication.java
+++ WillHero/src/main/java/iiitd/ac/ap_group17/willhero/HomeApplication.java
@@ -11,8 +11,7 @@ public class HomeApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HomeApplication.class.getResource("home.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
-
+        Scene scene = new Scene(fxmlLoader.load(), 1109, 794);
         stage.setTitle("Will Hero");
         stage.setFullScreen(false);
         stage.setResizable(false);
@@ -23,5 +22,10 @@ public class HomeApplication extends Application {
 
     public static void main(String[] args) {
         launch();
+    }
+
+    @Override
+    public void init() throws Exception {
+        super.init();
     }
 }
--- WillHero/src/main/java/iiitd/ac/ap_group17/willhero/HomeController.java
+++ WillHero/src/main/java/iiitd/ac/ap_group17/willhero/HomeController.java
@@ -1,14 +1,68 @@
 package iiitd.ac.ap_group17.willhero;
 
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
 
 public class HomeController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private  ImageView btnNewGame;
+
+    @FXML
+    private ImageView btnExit;
+
+    @FXML
+    private Label lbl;
+
+    @FXML
+    private ImageView btnSavedGame;
+
+    @FXML
+    private ImageView btnOption;
+
+    @FXML
+    protected void btnStartNewGameClicked(){
+            startButtonIllusionAnimation(btnNewGame, 115, 255);
+    }
+
+
+    @FXML
+    protected void btnSavedGameClicked() {
+            startButtonIllusionAnimation(btnSavedGame, 130, 190);
+    }
+
+
+    @FXML
+    protected void btnOptionClicked() {
+        startButtonIllusionAnimation(btnOption, 130, 190);
+    }
+
+    @FXML
+    protected void btnExitClicked() {
+        startButtonIllusionAnimation(btnExit, 130, 190);
+    }
+    private  void startButtonIllusionAnimation(ImageView btn, double height, double width) {
+
+        double _h = btn.getFitHeight();
+        double _w = btn.getFitWidth();
+        Timeline timeline = new Timeline();
+        timeline.setCycleCount(2);
+        timeline.setAutoReverse(true);
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(100), actionEvent -> {
+            btn.setFitHeight(height);
+            btn.setFitWidth(width);
+        }));
+
+        timeline.getKeyFrames().add(new KeyFrame(Duration.millis(200), actionEvent -> {
+            btn.setFitHeight(_h);
+            btn.setFitWidth(_w);
+        }));
+
+        timeline.play();
     }
 }
