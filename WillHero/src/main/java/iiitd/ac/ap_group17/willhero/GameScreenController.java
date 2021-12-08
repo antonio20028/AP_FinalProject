c817ecdaa2b3491386efff97e9ec6babdef80667
Antonio Pedro <antonio20028@iiitd.ac.in>
2021-12-08 21:34:44 +0530

    add Coin image

--- WillHero/src/main/java/iiitd/ac/ap_group17/willhero/GameScreenController.java
+++ WillHero/src/main/java/iiitd/ac/ap_group17/willhero/GameScreenController.java
@@ -1,4 +1,24 @@
 package iiitd.ac.ap_group17.willhero;
 
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
+
+import java.io.IOException;
+
 public class GameScreenController {
+    @FXML
+    private Button btnMenu;
+
+    @FXML
+    private AnchorPane gameScreen;
+
+
+    @FXML
+    protected void btnClicked() throws IOException {
+        AnchorPane inGameMenuScreen = FXMLLoader.load(getClass().getResource("inGameMenu.fxml"));
+
+        gameScreen.getChildren().setAll(inGameMenuScreen);
+    }
 }
